6ec324246c530d3be84070caa5007e8b31b60e7a
Dieu <47109555+AxelVitch@users.noreply.github.com>
2019-11-20 11:08:03 +0100

    la seécurité c'est bien mais le faire c'est mieux

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ else{
 //echo_session_id();
 date_default_timezone_set('Europe/Paris');
 include_once "functions/debug.php";
-
+include_once "functions/clean.php";
 include_once "includes/pdo.php";
 include_once "functions/displayTitle.php";
 include_once "includes/html.php";
