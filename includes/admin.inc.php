d5297a80340560ef3640890b5b72f9ed22acd5a5
Dieu <47109555+AxelVitch@users.noreply.github.com>
2019-11-25 16:34:54 +0100

    test

--- includes/admin.inc.php
+++ includes/admin.inc.php
@@ -441,7 +441,7 @@
 
     <div class="row">
         <div class="col-lg-12" >
-            &copy;  2014 yourdomain.com | Design by: <a href="http://binarytheme.com" style="color:#fff;" target="_blank">www.binarytheme.com</a>
+            &copy;  2014 yourdomain.com | Design by: <a href="http://binarytheme.com" style="color:#fff;" target="_blank"></a>
         </div>
     </div>
 </div>
